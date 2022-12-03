081c4045e6fa2c0c6955703034c44af2cb6aeab9
maqueise <caio.maqueise@gmail.com>
2022-12-02 23:11:14 -0400

    consumindo dados api

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
 import '../css/style.css'
+
+/* Selecionar os Elemento no DOM */
+const inputPesquisa = 
+document.querySelector("#inputPesquisa")
+const btnLocalizar = 
+document.querySelector("#btnLocalizar")
+const pokedexDisplay = 
+document.querySelector("#display")
+
+/* Adicionando Eventos */
+btnLocalizar.addEventListener('click',async function(){
+    /* buscar os dados do pokemon na api*/
+    const dadosDoPokemon = await localizarPokemon(inputPesquisa.value)
+    /* criar o cartão do Pokemon */
+    criarCartao(dadosDoPokemon)
+
+})
+
+
+async function localizarPokemon(termoBusca){
+  const url = 
+  `https://pokeapi.co/api/v2/pokemon/${termoBusca}`
+  const response = await fetch(url)
+  const pokemon =  await response.json()
+ //console.log(pokemon)
+  return pokemon
+}
+
+function criarCartao(pokemon){
+    const cartaoPokemon = document.createElement('div')
+    cartaoPokemon.className='cartaoPokemon'
+    cartaoPokemon.innerHTML =`
+    <img class="pokemonSprite" 
+    src="${pokemon.sprites.front_default}"/> 
+    <h2>${pokemon.name}</h2>
+    `
+    pokedexDisplay.appendChild(cartaoPokemon)
+}
